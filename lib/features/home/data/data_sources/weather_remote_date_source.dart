04551ab27bb4923afb8b8754bf9dedf38500200e
ahmad-hossi <ahmad.hessi.96@gmail.com>
2023-07-03 16:42:41 +0300

    add test files for weather feature

--- lib/features/home/data/data_sources/weather_remote_date_source.dart
+++ lib/features/home/data/data_sources/weather_remote_date_source.dart
@@ -6,6 +6,7 @@ import 'package:http/http.dart' as http;
 
 abstract class WeatherRemoteDataSource {
   Future<WeatherModel> getCurrentWeather(String requestParams);
+  Future<List<WeatherModel>> get5DaysWeather(String requestParams);
 }
 
 class WeatherRemoteDataSourceImpl implements WeatherRemoteDataSource {
@@ -28,6 +29,36 @@ class WeatherRemoteDataSourceImpl implements WeatherRemoteDataSource {
       if (response.statusCode == 200) {
         final responseData = json.decode(response.body);
         return WeatherModel.fromJson(responseData);
+      } else if (response.statusCode == 400) {
+        throw WrongLonOrLatException();
+      } else if (response.statusCode == 401) {
+        throw UnauthorizedException();
+      } else {
+        throw ServerException();
+      }
+    } catch (e) {
+      throw ServerException();
+    }
+  }
+
+  @override
+  Future<List<WeatherModel>> get5DaysWeather(String requestParams) async {
+    try {
+      final response = await _client.get(
+        Uri.parse(
+            '${AppConstants.baseUrl}/${AppConstants.fiveDaysWeatherEndPoint}'
+            '$requestParams'),
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+      );
+      print(response.body);
+      if (response.statusCode == 200) {
+        final responseData = json.decode(response.body);
+        return (responseData['list'] as List<dynamic>)
+            .map((e) => WeatherModel.fromJson(e as Map<String, dynamic>))
+            .toList();
       } else if (response.statusCode == 400) {
         throw WrongLonOrLatException();
       } else if (response.statusCode == 401) {
